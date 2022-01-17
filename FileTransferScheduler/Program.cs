80562e2fa36bd6f3d71d3150e1d18ba8f1d24701
vincent.chan <vincent.chan@anvione.com>
2022-01-17 14:34:53 +0800

    First Version

---
+++ FileTransferScheduler/Program.cs
@@ -0,0 +1,56 @@
+using FileTransferScheduler.Data;
+using FileTransferScheduler.Data.HostedService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileTransferScheduler
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(
+                options => options.AddFilter<EventLogLoggerProvider>(level => level >= LogLevel.Information)
+
+                )
+                //.ConfigureWebHostDefaults(webBuilder =>
+                //{
+                //    webBuilder.UseStartup<Startup>();
+                //})
+                .ConfigureServices((hostContext,services)=>
+                {
+                    var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    // load in the json file as configuration
+                    .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                    services.AddHostedService<TimeHostedService>()
+                    .Configure<EventLogSettings>(config =>
+                    {
+                        config.LogName = "File Transfer Log";
+                        config.SourceName = "File Transfer Source";
+
+
+                    });
+                    services.AddScoped<UploadService>();
+
+                })
+                .UseWindowsService();
+    }
+}
